7a798973539957f77c368d76834f23153810de20
Emma-Hyejin <cjd191058@naver.com>
2023-12-04 17:20:43 +0900

    Style: 마이페이지 UI 작업

---
+++ src/assets/svg/User.tsx
@@ -0,0 +1,14 @@
+import { SVG } from "@/types/svg";
+
+const User = ({ fillColor, width, height }: SVG) => {
+	fillColor = fillColor ? `${fillColor}` : "#000000";
+	return (
+		<svg fill={fillColor} width={width} height={height} viewBox="0 0 48 48">
+			<title />
+			<path d="M24,21A10,10,0,1,1,34,11,10,10,0,0,1,24,21ZM24,5a6,6,0,1,0,6,6A6,6,0,0,0,24,5Z" />
+			<path d="M42,47H6a2,2,0,0,1-2-2V39A16,16,0,0,1,20,23h8A16,16,0,0,1,44,39v6A2,2,0,0,1,42,47ZM8,43H40V39A12,12,0,0,0,28,27H20A12,12,0,0,0,8,39Z" />
+		</svg>
+	);
+};
+
+export default User;
