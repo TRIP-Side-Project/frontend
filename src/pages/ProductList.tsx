51766a6a8a128d1c07d238abb116671b21bf9b34
HyoJeong-Park <parkhj3003@gmail.com>
2023-12-08 21:31:08 +0900

    Style: 상품 목록 페이지 전체 레이아웃 1차 수정(검색어 마진 수정)

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,24 +1,43 @@
+import { useState } from "react";
+
 import Search from "@/components/search/Search";
 import ProductListItems from "@/components/productListItems/ProductListItems";
 import Pagenation from "@/components/Pagenation";
-export default function ProductList () {
+import { Link } from "react-router-dom";
+
+const ProductList = () => {
+
+  
+  // false -> 최신순 true -> 인기순
+  // 아무거나 눌러도 바뀌는 걸 만들어버렸다..
+  const [isSort, setIsSort] = useState(false);
+
+  const handleSort = () => {
+    setIsSort(!isSort);
+  }
+
+  const viewSortClass = "cursor-pointer px-2 text-BASIC_BLACK";
+  const nonViewSortClass = "cursor-pointer px-2 text-LIGHT_GRAY_COLOR";
+  
+
   return (
-    <>
-    <div className="px-28 bg-BASIC_WHITE">
-      <div className="flex justify-between align-center mb-10 pt-10">
-        <h1 className="text-3xl font-bold">눈꽃여행</h1>
+    <div className="px-28 bg-BASIC_WHITE w-full">
+      <div className="flex justify-between items-end py-20">
+        <h1 className="text-4xl font-bold">눈꽃여행</h1>
         <Search />
-        <div className="flex gap-2 border-b text-base">
-          <span>인기순</span>
-          <span> | </span>
-          <span>최신순</span>
+        <div className="flex gap-2 text-base border-b border-DARK_GRAY_COLOR px-3 pb-1">
+          <span className={isSort ? viewSortClass : nonViewSortClass} onClick={handleSort}>인기순</span>
+          <span className={!isSort ? viewSortClass : nonViewSortClass} onClick={handleSort}>최신순</span>
         </div>
       </div>
       {Array.from(Array(5), (_, index) => (
-				<ProductListItems key={index} />
+        <Link to={'/products/detail'}>
+          <ProductListItems key={index} />
+        </Link>
 			))}
       <Pagenation />
     </div>
-    </>
   )
 }
+
+export default ProductList;
