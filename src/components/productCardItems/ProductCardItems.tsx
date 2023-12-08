bd20857a0435a7fc7a89e5314180313535799a68
HyoJeong-Park <parkhj3003@gmail.com>
2023-12-08 21:59:21 +0900

    Style: 상품 목록 상세 전체 레이아웃 수정(뒤로가기버튼 구현)

--- src/components/productCardItems/ProductCardItems.tsx
+++ src/components/productCardItems/ProductCardItems.tsx
@@ -3,19 +3,19 @@ import jeju from "@/assets/img/jeju1.png";
 export default function ProductCardItems () {
   return (
     <>
-    <div className="bg-ITEM_BG_COLOR w-72 h-[360px] p-3 rounded-md shadow-md cursor-pointer">
-            <div className="w-full h-[220px] relative bg-rose-100 rounded-md overflow-hidden">
-              <div className="w-full h-full">
-                <img src={jeju} alt="jeju image" className="w-full h-full"/>
-              </div>
-              <p className="absolute bottom-0 right-0 bg-BASIC_BLACK text-BASIC_WHITE font-medium rounded-tl-md text-sm px-5 py-1">4박 5일패키지</p>
-            </div>
-            <div className="w-full pt-3 text-BASIC_BLACK">
-              <h3 className="text-2xl mb-2">Gyeong-ju</h3>
-              <p className="text-LIGHT_GRAY_COLOR">@아리아리랑 투어</p>
-              <span className="float-right mt-3">₩ 1,000,000원</span>
-            </div>
+      <div className="bg-ITEM_BG_COLOR w-72 h-[360px] p-3 rounded-md shadow-md cursor-pointer">
+        <div className="w-full h-[220px] relative bg-rose-100 rounded-md overflow-hidden">
+          <div className="w-full h-full">
+            <img src={jeju} alt="jeju image" className="w-full h-full"/>
           </div>
+          <p className="absolute bottom-0 right-0 bg-BASIC_BLACK text-BASIC_WHITE font-medium rounded-tl-md text-sm px-5 py-1">4박 5일패키지</p>
+        </div>
+        <div className="w-full pt-3 text-BASIC_BLACK">
+          <h3 className="text-2xl mb-2">Gyeong-ju</h3>
+          <p className="text-LIGHT_GRAY_COLOR">@아리아리랑 투어</p>
+          <span className="float-right mt-3">₩ 1,000,000원</span>
+        </div>
+      </div>
     </>
   )
 }
--- src/pages/ProductListDetail.tsx
+++ src/pages/ProductListDetail.tsx
@@ -4,6 +4,7 @@ import Button, { btnAttributes } from "@/common/button/Button";
 import Bookmark from "@/components/Bookmark/Bookmark";
 import ProductCardItems from "@/components/productCardItems/ProductCardItems";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ProductListDetail () {
 
@@ -52,24 +53,39 @@ export default function ProductListDetail () {
     }
     setIsBus(!isBus);
   }
-  
 
-    const selectVehicle: string = "w-1/2 px-3 py-1 text-BASIC_WHITE bg-MAIN_COLOR";
-    const normalVehicle: string = "w-1/2 px-3 py-1";
+  // 교통편
+  
+  const [depature, setDepature] = useState('');
+  const [arrival, setArrival] = useState('');
+
+  // const changeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   console.log(event.target.value);
+  // }
+  // console.log(depature);
+
+  // 뒤로가기 버튼
+  const navigate = useNavigate();
+  const backButton = () => {
+    navigate(-1);
+  }
+
+  const selectVehicle: string = "w-1/2 px-3 py-1 text-BASIC_WHITE bg-MAIN_COLOR";
+  const normalVehicle: string = "w-1/2 px-3 py-1";
 
 
   return (
     <>
-    <div className="px-28 pt-20 bg-BASIC_WHITE">
-      <div className="flex gap-1 mb-5 cursor-pointer align-top">
+    <div className="px-28 pt-20 bg-BASIC_WHITE w-full">
+      <div className="flex gap-1 mb-5 cursor-pointer items-top" onClick={backButton}>
         <p>목록 돌아가기</p>
         <BackToList fillColor="#333333" width="20px" height="20px" />
       </div>
-      <div className="border-b pb-10 border-LIGHT_GRAY_COLOR mb-20 flex h-[400px] align-center relative gap-10">
+      <div className="border-b pb-10 border-LIGHT_GRAY_COLOR mb-20 flex h-[400px] items-center relative gap-10">
         <div className="w-[400px] h-[300px] bg-rose-300 rounded-md">
           {/* 이미지 들어갈 자리 */}
         </div>
-        <div className="h-full flex flex-col justify-between align-center">
+        <div className="h-full flex flex-col justify-between items-center">
           <div>
             <h1 className="text-3xl font-bold mb-5">여행 상품명</h1>
             <p className="text-2xl mb-5">여행 상품 내용 어쩌구 쿵냐쿵 쿵냐리 쿵쿵</p>
@@ -99,7 +115,7 @@ export default function ProductListDetail () {
       </div>
       <div className="mb-20">
         <h1 className="text-3xl font-bold mb-5">관련 교통편</h1>
-        <div className="flex justify-between align-center text-lg font-bold">
+        <div className="flex justify-between items-start text-lg font-bold">
           <div className="text-center cursor-pointer w-[120px] flex flex-row rounded-md h-fit overflow-hidden bg-LINE_POINT_COLOR">
             <span className={(isTrain ? selectVehicle : normalVehicle)} onClick={handleisTrain}>기차</span>
             <span className={(isBus ? selectVehicle : normalVehicle)} onClick={handleisBus}>버스</span>
@@ -107,11 +123,11 @@ export default function ProductListDetail () {
           <div className="flex flex-col gap-5">
             <div>
               <span>출발</span>
-              <input type="text" className="ml-2 bg-LINE_POINT_COLOR rounded-md w-2/3 px-2 py-1 font-light" />
+              <input type="text" value={depature} onChange={(event) => {setDepature(event.target.value)}} className="ml-2 bg-LINE_POINT_COLOR rounded-md w-2/3 px-2 py-1 font-light" />
             </div>
             <div>
               <span>도착</span>
-              <input type="text" className="ml-2 bg-LINE_POINT_COLOR rounded-md w-2/3 px-2 py-1"/>
+              <input type="text" value={arrival} onChange={(event) => {setArrival(event.target.value)}} className="ml-2 bg-LINE_POINT_COLOR rounded-md w-2/3 px-2 py-1"/>
             </div>
           </div>
           <div className="flex flex-col gap-5">
@@ -128,7 +144,7 @@ export default function ProductListDetail () {
           </div>
           <Button btnInfo={searchBtnInfo} />
         </div>
-        <div className="w-full h-[200px] bg-LINE_POINT_COLOR rounded-md mt-5 flex justify-center align-center">
+        <div className="w-full h-[200px] bg-LINE_POINT_COLOR rounded-md mt-5 flex justify-center items-center">
           <div className="flex flex-col justify-around">
             <h3 className="text-xl font-bold">여행 일정을 공유해주세요!</h3>
             <div className="w-fit mx-auto">
@@ -139,7 +155,7 @@ export default function ProductListDetail () {
       </div>
       <div className="pb-20">
         <h1 className="text-3xl font-bold mb-5">관련상품</h1>
-        <div className="w-full flex justify-between align-center">
+        <div className="w-full flex justify-between">
           {Array.from(Array(3), (_, index) => (
             <ProductCardItems key={index} />
           ))}
