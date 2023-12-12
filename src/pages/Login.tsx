3f7ccc6e0ea91bebb2d948c8cb5b19d4a6c9f37f
HyoJeong-Park <parkhj3003@gmail.com>
2023-12-12 13:18:43 +0900

    Feat: 로그인 페이지 Input onchange 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,40 @@
 import Google from "@/assets/svg/Google";
 import kakao from "@/assets/img/kakao.png";
 import naver from "@/assets/img/naver.png";
+import { ChangeEvent, useState } from "react";
 
 export default function Login () {
+
   const loginInputClass = "pl-3 border-BASIC_BLACK w-full border h-12 rounded-md";
+
+  // 로그인 정보
+  interface loginData {
+    email: string,
+    password: string,
+  }
+
+  const [loginInfo, setLoginInfo] = useState<loginData>({
+    email: "",
+    password: ""
+  });
+
+  const changeLoginValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setLoginInfo({
+      ...loginInfo,
+      [event.target.name]: event.target.value,
+    })
+  }
+
   return(
     <>
     <div className="h-screen flex justify-center items-center">
       <div className="w-[700px] h-[600px] border border-BASIC_BLACK text-center flex flex-col items-center justify-center">
         <div className="w-1/2 flex flex-col justify-between items-center">
           <h1 className="text-2xl font-bold mb-7">로그인</h1>
-          <form action="#" acceptCharset="utf-8" name="login_form" method="post" className="w-full">
+          <form className="w-full">
             <div className="w-full flex flex-col items-center justify-between gap-5">
-              <input type="text" name="email" placeholder="이메일" className={loginInputClass}></input>
-              <input type="password" name="password" placeholder="비밀번호" className={loginInputClass}></input>
+              <input type="text" value={loginInfo.email} name="email" onChange={changeLoginValue} placeholder="이메일" className={loginInputClass}></input>
+              <input type="password" value={loginInfo.password} name="password" placeholder="비밀번호" onChange={changeLoginValue} className={loginInputClass}></input>
               <button className="text-xl font-bold w-full border h-12 rounded-md bg-BTN_COLOR text-BASIC_WHITE">로그인</button>
             </div>
           </form>
