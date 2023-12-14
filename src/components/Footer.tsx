05bf3995726f83de5f2cb528ca160dabad0530c5
HyoJeong-Park <parkhj3003@gmail.com>
2023-12-14 21:31:02 +0900

    Style: 푸터 반응형 작업

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
 const Footer = () => {
 	return (
-		<div className="w-full items-center h-[200px] border-t-2 border-LIGHT_GRAY_COLOR flex flex-row whitespace-nowrap">
-			<div className="text-4xl font-bold text-MAIN_COLOR px-9">TRIPTRIP</div>
+		<div className="w-full items-center py-8 md:py-0 md:h-[200px] border-t-2 border-LIGHT_GRAY_COLOR flex flex-col gap-7 md:gap-0 md:flex-row whitespace-nowrap">
+			<div className="text-3xl md:text-4xl font-bold text-MAIN_COLOR px-9">TRIPTRIP</div>
 			<div className="flex-1 font-light text-center text-LIGHT_GRAY_COLOR">
 				©TRIPTRIP. All Rights Reserved.
 			</div>
-			<div className="flex flex-col w-2/12 font-light text-LIGHT_GRAY_COLOR">
-				<div className="pb-3 text-center">GitHub</div>
-				<div className="flex flex-row justify-between ">
-					<ul className="px-3 text-center">
+			<div className="flex flex-col md:w-2/12 font-light text-LIGHT_GRAY_COLOR">
+				<div className="font-bold md:font-medium md:pb-3 text-left md:text-center">GitHub</div>
+				<div className="flex flex-col md:flex-row md:justify-between ">
+					<ul className="md:px-3 text-center flex gap-5 md:block md:gap-0">
 						<li>[FE]</li>
 						<li
 							className="cursor-pointer hover:text-BASIC_BLACK"
@@ -23,7 +23,7 @@ const Footer = () => {
 							박효정
 						</li>
 					</ul>
-					<ul className="px-3 text-center">
+					<ul className="md:px-3 text-center flex gap-5 md:block md:gap-0">
 						<li>[BE]</li>
 						<li
 							className="cursor-pointer hover:text-BASIC_BLACK"
