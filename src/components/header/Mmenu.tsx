e8f0fe93019fc77aa0d2214dbb1437f3024d8f70
HyoJeong-Park <parkhj3003@gmail.com>
2023-12-14 16:23:48 +0900

    Style: 헤더 반응형 작업

--- src/components/header/Mmenu.tsx
+++ src/components/header/Mmenu.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import Hamburger from "@/assets/svg/Hamburger";
 import Close from "@/assets/svg/Close";
+import { useNavigate } from "react-router-dom";
 
 type NavTypes = {
 	openNav: (trip: null | string) => void;
+	onSubMenuClose: () => void;
 };
 
-const Mmenu = ({ openNav }: NavTypes) => {
+const Mmenu = ({ openNav, onSubMenuClose }: NavTypes) => {
 	const [isOpen, setIsOpen] = useState(false);
+	
+	const navigator = useNavigate();
+	const moveToForum = () => {
+		navigator('/forum');
+	}
 
 	return (
 		<div className="md:hidden">
@@ -21,20 +28,33 @@ const Mmenu = ({ openNav }: NavTypes) => {
 				</button>
 			</div>
 			{isOpen && (
-				<div className="absolute left-0 flex flex-col w-screen shadow-md bg-BASIC_WHITE top-32">
+				<div className="z-10 absolute left-0 flex flex-col w-screen shadow-md bg-BASIC_WHITE top-32">
 					<button
 						className="py-8 border-b font-xl border-LIGHT_GRAY_COLOR"
-						onClick={() => openNav("region")}
+						onClick={() => {
+							openNav("region");
+							setIsOpen(!isOpen);
+						}}
 					>
 						지역별 여행
 					</button>
 					<button
 						className="py-8 border-b font-xl border-LIGHT_GRAY_COLOR"
-						onClick={() => openNav(null)}
+						onClick={() => {
+							openNav(null);
+							setIsOpen(!isOpen);
+						}}
 					>
 						테마별 여행
 					</button>
-					<button className="py-8 font-xl" onClick={() => openNav("/forum")}>
+					<button
+						className="py-8 font-xl" 
+						onClick={() => {
+							// openNav("/forum");
+							setIsOpen(!isOpen);
+							onSubMenuClose();
+							moveToForum();
+						}}>
 						여행 포럼
 					</button>
 				</div>
