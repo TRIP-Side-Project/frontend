0a3278b01f26a6f3f1d0e51d2e7b10ab1e9debbc
HyoJeong-Park <parkhj3003@gmail.com>
2023-12-28 20:16:23 +0900

    Style: 교통편 리액트 쿼리 적용

--- src/components/searchVehicle/SearchVehicle.tsx
+++ src/components/searchVehicle/SearchVehicle.tsx
@@ -1,7 +1,8 @@
 import Train from "@/assets/svg/Train";
 import Button, { btnAttributes } from "@/common/button/Button";
+import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { ChangeEvent, FormEventHandler, useCallback, useEffect, useState } from "react";
+import { ChangeEvent, FormEventHandler, useEffect, useState } from "react";
 
 const SearchVehicle = () => {
 
@@ -22,8 +23,8 @@ const SearchVehicle = () => {
   }
 
   interface locationNumInfo {
-    depature: number | null;
-    arrival: number | null;
+    depatureNum: number | null;
+    arrivalNum: number | null;
   }
 
   const searchBtnInfo: btnAttributes = {
@@ -34,8 +35,8 @@ const SearchVehicle = () => {
   }
   
   // style
-  const locationStyle = "ml-10 md:ml-2 bg-LINE_POINT_COLOR rounded-md w-[150px] px-2 py-1 font-light focus:outline-MAIN_COLOR";
-  const selectVehicle: string = "px-3 py-1 text-BASIC_WHITE bg-MAIN_COLOR";
+  const locationStyle = "ml-10 md:ml-2 bg-LINE_POINT_COLOR rounded-md w-[190px] px-2 py-1 font-light focus:outline-MAIN_COLOR";
+  const selectVehicle = "px-3 py-1 text-BASIC_WHITE bg-MAIN_COLOR w-full text-center";
   
   // 달력
   const today = new Date();
@@ -57,8 +58,8 @@ const SearchVehicle = () => {
   });
 
   const [numLocation, setNumLocation] = useState<locationNumInfo>({
-    depature: null,
-    arrival: null,
+    depatureNum: null,
+    arrivalNum: null,
   });
 
   const changeLocationValue = (event:ChangeEvent<HTMLInputElement>) => {
@@ -96,23 +97,27 @@ const SearchVehicle = () => {
 
   const [depatureDataArray, setDepatureDataArray] = useState<DepatureData>({terminalData: []});
 
-
-  const findTerNum = useCallback(async () => {
-    const fetchData = async () => {
-      try {
-        const ter_numResponse = await axios.get(
-          `https://apigw.tmoney.co.kr:5556/gateway/koLoadInfo/v1/load_info/010`,
-          // 개발환경에서는 /proxy 로
-          {
-            headers: {
-              "x-Gateway-APIKey": "d2338a0c-d836-421f-aaa3-0679c3884a4a"
-            }
+  const findMutation = useMutation({
+    mutationFn: () => {
+      return axios.get(
+        `/proxy/koLoadInfo/v1/load_info/010`,
+        // 개발환경에서는 /proxy 로
+        {
+          headers: {
+            "x-Gateway-APIKey": "d2338a0c-d836-421f-aaa3-0679c3884a4a"
           }
-        );
-        const listData = ter_numResponse.data.response.ter_list;
-        const depatureData = [listData.filter((item: ListItem) => item.TER_NAM.includes(location.depature))];
-        const arrivalData = [listData.filter((item: ListItem) => item.TER_NAM.includes(location.arrival))];
-        // console.log(depatureData[0][0].TER_COD);
+        }
+      );
+    }
+  })
+
+  const findTerNum = async () => {
+      try {
+        const response = await findMutation.mutateAsync();
+        const responseData = response.data.response.ter_list;
+        const depatureData = [responseData.filter((item: ListItem) => item.TER_NAM.includes(location.depature))];
+        const arrivalData = [responseData.filter((item: ListItem) => item.TER_NAM.includes(location.arrival))];
+        console.log(response.data.response.ter_list);
         const depatureTerminalData = depatureData[0].map((item: ListItem) => ({
           depatureTerminal: item.TER_NAM,
         }));
@@ -130,39 +135,42 @@ const SearchVehicle = () => {
 
         setNumLocation({
           ...numLocation,
-          depature: depatureData[0][0].TER_COD,
-          arrival: arrivalData[0][0].TER_COD
+          depatureNum: depatureData[0][0].TER_COD,
+          arrivalNum: arrivalData[0][0].TER_COD
         });
       } catch (error) {
         console.log("Error : " + error);
         // alert("시스템 점검 중 입니다!");
       }
     };
-
-    fetchData();
-  }, [location, setNumLocation]); // 함수 내부에서 사용되는 의존성 추가
+  
   useEffect(() => {
     findTerNum();
   }, [location])
-  console.log(depatureDataArray);
-  // console.log(numLocation);
   // 청주가 들어가는 검색어로 입력 *청주* 이런 느낌으로 해야함
   // 검색어 드롭다운으로 미리 보여주고 선택하게 해도 좋을 듯
+  console.log(numLocation);
+  console.log(location);
 
-  const handleVehicle:FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault();
-    setLineList(false);
-    const fetchData = async () => {
-      try{
-        const searchDate = selectedDate.split("-").join("");
-        const searchTime = selectedTime.split(":").join("");
-        const searchResponse = await axios.get(
-          `/koIbtList/v1/ibt_list/${searchDate}/${searchTime}/${numLocation.depature}/${numLocation.arrival}/0/0/9`,
+  const searchMutation = useMutation({
+    mutationFn: () => {
+      const searchDate = selectedDate.split("-").join("");
+      const searchTime = selectedTime.split(":").join("");
+      return axios.get(
+          `/proxy/koIbtList/v1/ibt_list/${searchDate}/${searchTime}/${numLocation.depatureNum}/${numLocation.arrivalNum}/0/0/9`,
           {
           headers: {
             "x-Gateway-APIKey": "f7f4cfab-c6e5-4c2d-ba83-283349af63e4"
           }
         })
+    }
+  });
+
+  const handleVehicle:FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+    setLineList(false);
+      try{
+        const searchResponse = await searchMutation.mutateAsync();
         if(searchResponse.data.message === "정상"){
           setLineList(true);
           const data = searchResponse.data.response.line_list;
@@ -182,15 +190,14 @@ const SearchVehicle = () => {
         console.log("Error : " + error);
       }
     }
-    fetchData();
-  }
+    console.log(depatureDataArray);
 
   return (
     <div className="mb-20">
         <h1 className="text-2xl md:text-3xl font-bold mb-5">관련 교통편</h1>
         <div className="">
           <form onSubmit={handleVehicle} className="flex flex-col md:flex-row gap-10 md:gap-0 justify-between items-start text-lg font-bold">
-            <div className="text-center cursor-pointer rounded-md overflow-hidden">
+            <div className="cursor-pointer overflow-hidden rounded-md w-[100px] border flex justify-center items-center">
               <span className={selectVehicle}>버스</span>
             </div>
             <div className="flex flex-col gap-5">
@@ -205,8 +212,6 @@ const SearchVehicle = () => {
             </div>
             <div className="flex flex-col gap-5">
               <div>
-                {/* 디테일한 디자인 생각 좀 해보고 구현할게요. */}
-                {/* 날짜 선택 스크립트 구현 예정 */}
                 <span>날짜</span>
                 <input type="date" value={selectedDate} onChange={handleDateChange} className={locationStyle}/>
               </div>
@@ -222,7 +227,7 @@ const SearchVehicle = () => {
         </div>
         <div className={`w-full h-[200px] bg-LINE_POINT_COLOR rounded-md mt-5 flex justify-center items-center relatvive ${lineList && "overflow-y-scroll"}`}>
           <div className="w-full h-full">
-            {!lineList && noLineMessage && <h3 className="flex justify-center items-center">{noLineMessage}</h3>}
+            {!lineList && noLineMessage && <h3 className="flex justify-center items-center h-full">{noLineMessage}</h3>}
             {!lineList && !noLineMessage && 
               <div className="w-full h-full flex flex-col justify-center items-center">
                 <h3 className="text-xl font-bold">여행 일정을 공유해주세요!</h3>
