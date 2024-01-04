963ba54a6e8953ca77daeb68e119bf569771a32c
Emma-Hyejin <cjd191058@naver.com>
2024-01-04 22:52:43 +0900

    Fix: 댓글 컴포넌트 memberId 로컬에서 불러와서 사용 및 에러 페이지 수정

--- src/components/Loading/ErrState.tsx
+++ src/components/Loading/ErrState.tsx
@@ -1,4 +1,5 @@
-import Whale from "@/assets/img/Whale.png";
+// import Whale from "@/assets/img/Whale.png";
+import err404 from "@/assets/img/error404.jpg";
 interface errProps {
 	err: string;
 }
@@ -6,10 +7,12 @@ interface errProps {
 const ErrState = ({ err }: errProps) => {
 	return (
 		<div className="w-full h-screen pt-20 overflow-y-auto text-center dark:bg-BASIC_BLACK">
-			<img src={Whale} className="scale-50" alt="데이터 불러오기 실패 " />
-			<div className="font-mono text-lg font-semibold">
-				현재 네트워크가 불안해욥! 데이터를 불러오지 못했어요
-				<span>{err}</span>
+			<div className="flex flex-col justify-center">
+				<img src={err404} className="scale-50" alt="데이터 불러오기 실패 " />
+				<div className="font-mono text-lg font-semibold">
+					아래와 같은 문제로 데이터를 불러오지 못했어요
+				</div>
+				<div className="font-mono font-semibold text-md">{err}</div>
 			</div>
 		</div>
 	);
