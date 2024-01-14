a7a211e226adeb52fbe258fd7b54bf190ddf65ad
Emma-Hyejin <cjd191058@naver.com>
2024-01-15 01:10:47 +0900

    Feat: 마이페이지 레이아웃 수정

--- src/components/mineItems/LikeForum.tsx
+++ src/components/mineItems/LikeForum.tsx
@@ -1,24 +1,71 @@
+import Close from "@/assets/svg/Close";
 import Category from "@/common/category/Category";
+import useFormatDate from "@/hooks/useFormatDate";
 import { LikeForumProps } from "@/types/myProfile";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 const LikeForum = ({ data }: LikeForumProps) => {
 	console.log(data);
-	//gap-3
-	return (
-		<div className="flex flex-col w-full gap-3 px-3 py-2 border rounded-lg whitespace-nowrap bg-BASIC_WHITE">
-			<Link to={`/forum/detail/${data.articleId}`}>
-				<div className="relative flex flex-row justify-between text-xs">
-					<div className="relative scale-75 -left-3 -top-1">
-						<Category isEditor={data.writerRole} />
-					</div>
+	const formattedDate = useFormatDate(data && data.createdAt);
+	const queryClient = useQueryClient();
+	const BASE_URL = import.meta.env.VITE_BASE_URL;
+	const ACCESS_TOKEN = window.localStorage.getItem("access_token");
 
-					<div className=" text-LIGHT_GRAY_COLOR">{data.createdAt}</div>
-				</div>
-				<div className="text-lg font-semibold text-BASIC_BLACK">
-					{data.title}
-				</div>
-			</Link>
+	const handleDeleteMine = useMutation({
+		mutationFn: (interestArticleId: number) => {
+			return axios.delete(
+				`${BASE_URL}/api/interest-articles/${interestArticleId}`,
+				{
+					headers: {
+						accessToken: `Bearer ${ACCESS_TOKEN}`,
+					},
+				},
+			);
+		},
+		onSettled: () => queryClient.invalidateQueries({ queryKey: ["mineDatas"] }),
+	});
+
+	const DeleteMineClick = async (interestArticleId: number) => {
+		try {
+			await handleDeleteMine.mutateAsync(interestArticleId);
+		} catch (err) {
+			throw new Error(`내 아이템 삭제 ${err}`);
+		}
+	};
+
+	return (
+		<div className="flex flex-row w-full px-3 py-2 border rounded-lg whitespace-nowrap bg-BASIC_WHITE">
+			<div className="flex flex-col flex-1 gap-3 ">
+				<Link to={`/forum/detail/${data.articleId}`}>
+					<div className="relative flex flex-row justify-between flex-1 text-xs">
+						<div className="relative scale-75 -left-3 -top-1">
+							<Category isEditor={data.writerRole} />
+						</div>
+
+						<div className=" text-LIGHT_GRAY_COLOR">{formattedDate}</div>
+					</div>
+					<div className="text-lg font-semibold text-BASIC_BLACK">
+						{data.title}
+					</div>
+				</Link>
+			</div>
+			<div className=" text-BASIC_BLACK">
+				<button
+					type="button"
+					className="ms-auto -mx-1  -my-1.5 justify-center items-center flex-shrink-0 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+					data-dismiss-target="#toast-notification"
+					aria-label="Close"
+					onClick={(e) => {
+						e.preventDefault();
+						console.log(data.articleId);
+						DeleteMineClick(data.articleId);
+					}}
+				>
+					<Close fillColor={"#111111"} width={"18px"} height={"18px"} />
+				</button>
+			</div>
 		</div>
 	);
 };
--- src/components/mineItems/LikeItem.tsx
+++ src/components/mineItems/LikeItem.tsx
@@ -1,64 +1,70 @@
 import Close from "@/assets/svg/Close";
 import { LikeMerchanProps } from "@/types/myProfile";
 import { useNavigate } from "react-router-dom";
-// import { useMutation } from "@tanstack/react-query";
-// import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
 const Likeitem = ({ data }: LikeMerchanProps) => {
 	const navigation = useNavigate();
-	// const BASE_URL = import.meta.env.VITE_BASE_URL;
-	// const ACCESS_TOKEN = window.localStorage.getItem("access_token");
+	const BASE_URL = import.meta.env.VITE_BASE_URL;
+	const ACCESS_TOKEN = window.localStorage.getItem("access_token");
 
-	// const handleDeleteMine = useMutation({
-	// 	mutationFn: (interesItemId: number) => {
-	// 		return axios.delete(`${BASE_URL}/api/interest-items/${interesItemId}`, {
-	// 			headers: {
-	// 				accessToken: `Bearer ${ACCESS_TOKEN}`,
-	// 			},
-	// 		});
-	// 	},
-	// });
+	const handleDeleteMine = useMutation({
+		mutationFn: (interesItemId: number) => {
+			return axios.delete(`${BASE_URL}/api/interest-items/${interesItemId}`, {
+				headers: {
+					accessToken: `Bearer ${ACCESS_TOKEN}`,
+				},
+			});
+		},
+	});
 
-	// const DeleteMineClick = async (interesItemId: number) => {
-	// 	try {
-	// 		await handleDeleteMine.mutateAsync(interesItemId);
-	// 	} catch (err) {
-	// 		throw new Error(`내 아이템 삭제 ${err}`);
-	// 	}
-	// };
+	const DeleteMineClick = async (interesItemId: number) => {
+		try {
+			await handleDeleteMine.mutateAsync(interesItemId);
+		} catch (err) {
+			throw new Error(`내 아이템 삭제 ${err}`);
+		}
+	};
 	console.log(data);
 	return (
-		<div
-			className="flex flex-row w-full p-2 border rounded-lg whitespace-nowrap bg-BASIC_WHITE"
-			onClick={() => navigation(`/products/detail/${data && data.id}`)}
-		>
-			<img
-				src={data.imageUrl}
-				className="rounded-lg w-[100px] sm:w-[130px] h-[100px] mr-3"
-			/>
-			<div className="flex flex-col flex-1 text-BASIC_BLACK">
-				<div className="flex flex-row justify-between ">
-					<div className="text-sm font-semibold whitespace-normal sm:text-lg">
-						{data.title.slice(0, 28)}..
-					</div>
-					<button
-						type="button"
-						className="ms-auto -mx-1.5  -my-1.5 justify-center items-center flex-shrink-0 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-						data-dismiss-target="#toast-notification"
-						aria-label="Close"
-						onClick={(e) => {
-							e.preventDefault();
-							// DeleteMineClick(data.id);
-						}}
-					>
-						<Close fillColor={"#111111"} width={"18px"} height={"18px"} />
-					</button>
-				</div>
+		<div className="flex flex-row w-full p-2 border rounded-lg whitespace-nowrap bg-BASIC_WHITE">
+			<div
+				className="flex flex-row flex-1"
+				onClick={() => navigation(`/products/detail/${data && data.id}`)}
+			>
+				<img
+					src={data.imageUrl}
+					className="rounded-lg w-[100px] sm:w-[130px] h-[100px] mr-3"
+				/>
 
-				<div className="flex-1 text-sm text-LIGHT_GRAY_COLOR">
-					{data.shopName}
+				<div className="flex flex-col ">
+					<div className="text-sm font-semibold whitespace-normal sm:text-lg">
+						{data.title.slice(0, 30)}..
+					</div>
+					<div className="flex-1 text-sm text-LIGHT_GRAY_COLOR ">
+						{data.shopName}
+					</div>
+					<div className="pr-4 text-sm text-right">
+						{String(data.minPrice).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원
+					</div>
 				</div>
-				<div className="flex justify-end text-base">{data.maxPrice}</div>
+			</div>
+
+			<div className="text-BASIC_BLACK">
+				<button
+					type="button"
+					className="ms-auto -mx-1  -my-1.5 justify-center items-center flex-shrink-0 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+					data-dismiss-target="#toast-notification"
+					aria-label="Close"
+					onClick={(e) => {
+						e.preventDefault();
+						console.log(data.id);
+						DeleteMineClick(data.id);
+					}}
+				>
+					<Close fillColor={"#111111"} width={"18px"} height={"18px"} />
+				</button>
 			</div>
 		</div>
 	);
